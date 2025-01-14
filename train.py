5ac63f682420cd5defc1f57d072dbc2ec8e6b25d
pokabu1999 <thang15021999@gmail.com>
2025-01-14 12:47:55 +0700

    fix: update code

--- train.py
+++ train.py
@@ -187,6 +187,6 @@ for dataset in tqdm(
                 model_selection_spear = np.mean(val_spears)
                 torch.save(model.state_dict(), "CSTA/tmp/weight.pt")
         shutil.move(
-            "./tmp/weight.pt",
+            "CSTA/tmp/weight.pt",
             f"drive/MyDrive/research/paper2/weights/{dataset}/split{split_id+1}.pt",
         )
